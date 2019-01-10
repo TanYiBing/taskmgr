8c81a7e1844a70553d90ba258636a088a19f304f
TanYiBing <820567197@qq.com>
2019-01-10 13:12:57 +0800

    支持svg版的mat-icon

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,24 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { loadSvgResources } from '../utils/svg.util';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     HeaderComponent,
@@ -20,12 +27,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   ]
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+  constructor(
+    @Optional() @SkipSelf() parentModule: CoreModule,
+    ir: MatIconRegistry,
+    ds: 　DomSanitizer
+  ) {
     if (parentModule) {
       throw new Error(
         'CoreModule is already loaded. Import it in the AppModule only'
       );
-
     }
+    loadSvgResources(ir, ds);
   }
 }
---
+++ src/app/utils/svg.util.ts
@@ -0,0 +1,6 @@
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+
+export const loadSvgResources = (ir: MatIconRegistry, ds:　DomSanitizer) => {
+  const imgDir = 'assests/img';
+};
