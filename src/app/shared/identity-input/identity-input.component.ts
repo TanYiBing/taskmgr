c5497ffa1f5dfb60658f4f6fbd1f1acfc2079e95
TanYiBing <820567197@qq.com>
2019-01-21 11:00:48 +0800

    完成身份输入模块

--- src/app/shared/identity-input/identity-input.component.ts
+++ src/app/shared/identity-input/identity-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, forwardRef, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { NG_VALUE_ACCESSOR, NG_VALIDATORS, ControlValueAccessor } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, NG_VALIDATORS, ControlValueAccessor, FormControl } from '@angular/forms';
 import { IdentityType, Identity } from '../../domain';
 import { Subject, Observable, Subscription, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -59,8 +59,12 @@ export class IdentityInputComponent implements OnInit, ControlValueAccessor, OnD
       this._sub.unsubscribe();
     }
   }
-  public writeValue(obj): void {
 
+  // 设置初始值
+  public writeValue(obj): void {
+    if (obj) {
+      this.identity = obj;
+    }
   }
 
   public registerOnChange(fn: any): void {
@@ -70,13 +74,70 @@ export class IdentityInputComponent implements OnInit, ControlValueAccessor, OnD
   // 这里没有使用，用于注册 touched 状态
   public registerOnTouched(fn: any): void {
   }
-
   public setDisabledState?(isDisabled: boolean): void {
   }
 
   // 验证表单，验证结果正确返回 null 否则返回一个验证结果对象
-  validate(c: FormControl) {
+  validate(c: FormControl): { [key: string]: any } | null  {
+    if (!c.value) {
+      return null;
+    }
+    switch (c.value.identityType) {
+      case IdentityType.IdCard: {
+        return this.validateIdNumber(c);
+      }
+      case IdentityType.Passport: {
+        return this.validatePassport(c);
+      }
+      case IdentityType.Military: {
+        return this.validateMilitary(c);
+      }
+      case IdentityType.Insurance:
+      default: {
+        return null;
+      }
+    }
+  }
 
+  validateIdNumber(c: FormControl): { [key: string]: any } | null  {
+    const val = c.value.identityNo;
+    if (val.length !== 18) {
+      return {
+        idNotValid: true
+      };
+    }
+    const pattern = /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}[x0-9]$/;
+    const result = pattern.test(val);
+    // if (pattern.test(val)) {
+    //   const info = extractInfo(val);
+    //   if (isValidAddr(info.addrCode) && isValidDate(info.dateOfBirth)) {
+    //     result = true;
+    //   }
+    // }
+    return result ? null : { idNotValid: true };
+  }
+
+  validateMilitary(c: FormControl): { [key: string]: any } | null {
+    const value = c.value.identityNo;
+    const pattern = /[\u4e00-\u9fa5](字第)(\d{4,8})(号?)$/;
+    let result = false;
+    if (pattern.test(value)) {
+      result = true;
+    }
+    return result ? null : { idNotValid: true };
+  }
+
+  validatePassport(c: FormControl): { [key: string]: any } | null {
+    const value = c.value.identityNo;
+    if (value.length !== 9) {
+      return { idNotValid: true };
+    }
+    const pattern = /^[GgEe]\d{8}$/;
+    let result = false;
+    if (pattern.test(value)) {
+      result = true;
+    }
+    return result ? null : { idNotValid: true };
   }
 
   onIdTypeChange(idType: IdentityType) {
